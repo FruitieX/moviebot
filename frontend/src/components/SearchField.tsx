bc8b8258a9ca3de15380558aa2300dc832b1222e
oscar henriksson <oscar.henriksson@aalto.fi>
2018-11-15 18:56:40 +0200

    changed input to form

--- frontend/src/components/SearchField.tsx
+++ frontend/src/components/SearchField.tsx
@@ -1,17 +1,15 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {
-  handleChange: (value: string) => void
-  value: string
+  handleChange: (value: string) => void;
+  value: string;
 }
 export class SearchField extends React.Component<Props> {
   onChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    this.props.handleChange(event.currentTarget.value)
+    this.props.handleChange(event.currentTarget.value);
 
   render() {
     return (
-      <div>
-        <input onChange={this.onChange} value={this.props.value} />
-      </div>
-    )
+      <input type="text" onChange={this.onChange} value={this.props.value} />
+    );
   }
 }
--- frontend/src/components/SearchMovie.tsx
+++ frontend/src/components/SearchMovie.tsx
@@ -1,29 +1,30 @@
-import * as React from 'react'
-import { SearchField } from './SearchField'
-import { Button } from './Button'
+import * as React from 'react';
+import { SearchField } from './SearchField';
+import { Button } from './Button';
 
 export class SearchMovie extends React.Component {
   state = {
     searchMovie: '',
     resultMovies: [''],
-  }
+  };
 
   handleChange = (value: string) => {
-    this.setState({ searchMovie: value })
-  }
-  handleSearch = () => {
-    alert(this.state.searchMovie)
-  }
+    this.setState({ searchMovie: value });
+  };
+  handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(this.state.searchMovie);
+  };
 
   render() {
     return (
-      <>
+      <form onSubmit={this.handleSearch}>
         <SearchField
           handleChange={this.handleChange}
           value={this.state.searchMovie}
         />
-        <Button name="search" onClick={this.handleSearch} />
-      </>
-    )
+        <Button name="search" />
+      </form>
+    );
   }
 }
