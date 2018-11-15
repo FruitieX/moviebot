fdaeb8682db177add76006db25e72674c22b494f
Rasmus Eskola <fruitiex@gmail.com>
2018-11-15 18:32:55 +0200

    Print which port server listens on, allow overriding port with envvar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,4 +25,6 @@ createConnection()
   })
   .catch(error => console.log(error));
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log(`listening on ${PORT}`);
