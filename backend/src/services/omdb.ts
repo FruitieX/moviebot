8736b2f0bffb03e4462b6dfbc33ffa67c9a89814
Rasmus Eskola <fruitiex@gmail.com>
2018-11-15 21:38:24 +0200

    Implement movie search route & service

---
+++ backend/src/services/omdb.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const BASE_URL = 'https://www.omdbapi.com';
+
+const omdb = axios.create({
+  baseURL: BASE_URL,
+
+  // TODO: why this no work
+  /*
+  params: {
+    apikey: process.env.OMDB_API_KEY
+  }
+  */
+})
+
+export const search = async (query: string) => {
+  const response = await omdb.get('/', {
+    params: {
+      t: query,
+      apikey: process.env.OMDB_API_KEY
+    }
+  });
+
+  return response.data;
+};
+
+export default omdb;
